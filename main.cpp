e6ee2d00a2cc67f2439e22960404b1b2b46dd41e
Ericawat <118088300+Ericawat@users.noreply.github.com>
2023-05-02 17:38:23 -0700

    Normal code of the program written and ready for testing :) next we'll work on adding some good logics as to make the code advance an at the same time understandable to follow with or without comments :)
    i hate commenting codes :))

--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout<<" \t ******************* WEEK DAYS ******************* \n \n "<<endl;
+
+    cout<<"hello and welcome to this simple skill testing program"<<endl;
+
+    int days;
+
+    cout<<"Enter a number between 1 - 7 to know its correspondent in Days of the week :) "<<endl;
+    cin>>days;
+
+    switch (days) {
+
+        case 1:
+            cout<<"monday "<<endl;
+            break;
+        case 2:
+            cout<<"tuesday "<<endl;
+            break;
+        case 3:
+            cout<<"Wednesday "<<endl;
+            break;
+        case 4:
+            cout<<"Thursady "<<endl;
+            break;
+        case 5:
+            cout<<"Friday "<<endl;
+            break;
+        case 6:
+            cout<<"Saturday "<<endl;
+            break;
+        case 7:
+            cout<<"Sunday "<<endl;
+            break;
+
+        default:
+            cout << "Please Enter a valid day within the range 1 through 7 : Thank-u :" << endl;
+    }
+
+
     return 0;
 }
